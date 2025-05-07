74f804f3fc644c925bfebae7d91226ae771acfc1
goake <glen@omc-ltd.com>
2025-05-07 00:46:45 +0000

    Authorisation setup

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import { authMiddleware } from "./middleware/authMiddleware"
+/* ROUTE IMPORT */
+import tenantRoutes from "./routes/tenantRoutes"
+import managerRoutes from "./routes/managerRoutes"
+
+/* CONFIGURATION */
+dotenv.config({ path: require("find-config")(".env") })
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("This is home route")
+})
+
+app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes)
+app.use("/managers", authMiddleware(["manager"]), managerRoutes)
+
+/* SERVER */
+const port = Number(process.env.PORT) || 3002
+app.listen(port, () => {
+  console.log(`Server runnning on port ${port}`)
+})
